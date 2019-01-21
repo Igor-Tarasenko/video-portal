cc406b881fc7d6698c442e4120577e85f9220a27
Igor Tarasenko <tarasenko.igr@gmail.com>
2019-01-21 14:11:43 +0200

    add registred page

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('input:checkbox').styler();
+    $('input:checkbox, select').styler();
     $('.page-link').click(function () {
        $('.pagination').find('.page-link').removeClass('active');
        $(this).addClass('active');
